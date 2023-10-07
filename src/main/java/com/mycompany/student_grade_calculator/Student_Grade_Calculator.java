7e205f99fe9504f0dd677362c67a854566f8b79d
RAHUL SHARMA <9936rdsharma@gmail.com>
2023-10-07 11:49:24 +0530

    First Commit

---
+++ src/main/java/com/mycompany/student_grade_calculator/Student_Grade_Calculator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.student_grade_calculator;
+
+/**
+ *
+ * @author RAHUL SHARMA
+ */
+public class Student_Grade_Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
